50900dcf62628c86ae12fe097ddc5f9c41c25e11
Ryan Nemiroff <ryguyn@gmail.com>
2020-06-24 02:03:26 -0700

    Clean out build files

--- Projects/Multi/Examples/MotionControl/IHM01A1_ExampleFor1Motor/SW4STM32/STM32F401RE-Nucleo/Debug/Drivers/BSP/X-NUCLEO-IHMxx/x_nucleo_ihmxx.d
+++
@@ -1,8 +0,0 @@
-Drivers/BSP/X-NUCLEO-IHMxx/x_nucleo_ihmxx.o: \
- /Users/wkaiser/Documents/STMicroelectronics_Motor_Control_Curriculum/Edukit_Resources/Edukit_Firmware/Release_6-10-2020/Project/MC_curriculum/Drivers/BSP/X-NUCLEO-IHMxx/x_nucleo_ihmxx.c \
- /Users/wkaiser/Documents/STMicroelectronics_Motor_Control_Curriculum/Edukit_Resources/Edukit_Firmware/Release_6-10-2020/Project/MC_curriculum/Drivers/BSP/X-NUCLEO-IHMxx/x_nucleo_ihmxx.h \
- ../../../../../../../../Drivers/BSP/Components/Common/motor.h
-
-/Users/wkaiser/Documents/STMicroelectronics_Motor_Control_Curriculum/Edukit_Resources/Edukit_Firmware/Release_6-10-2020/Project/MC_curriculum/Drivers/BSP/X-NUCLEO-IHMxx/x_nucleo_ihmxx.h:
-
-../../../../../../../../Drivers/BSP/Components/Common/motor.h:
